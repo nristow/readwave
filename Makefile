b4eb9a660e9b0bb3eff9f73f25b414eaeaea6ce5
Nick Ristow <nick.ristow@gmail.com>
2019-03-26 16:34:06 -0400

    Inverted waveforms, trying different fits

--- Makefile
+++ Makefile
@@ -5,5 +5,9 @@ INCROOT2="/usr/include/root"
 default: 
 	g++ -g `root-config --libs --glibs ` -I/usr/include/root -I/usr/include/root/ROOT readWave.cpp -o readWave
 
+run:
+	#`readWave ../waveform_1553105709/` 
+	root PMT_fit.root
+
 clean:
-	rm -f readWave
+	rm -f readWave *.png
--- readWave.cpp
+++ readWave.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include <deque>
 #include <algorithm>
 #include <numeric>
 #include <cmath>
@@ -15,6 +16,7 @@
 #include <root/TCanvas.h>
 #include <root/THStack.h>
 #include <root/TF1.h>
+#include <root/TFile.h>
 
 struct package
 {
@@ -32,8 +34,6 @@ double Gaus_2sigma( double *x, double*par )
 	double sigma_1 = par[2];
 	double sigma_2 = par[3];
 	 
-	double Pol = par[4] + xx*(par[5] + xx*par[6] );
-	  
 	if( xx < mean ) 
 	{
 		return height*exp( -(xx - mean)*(xx - mean)/(2*sigma_1*sigma_1) );
@@ -54,7 +54,7 @@ Double_t gaus_lau (Double_t *x, Double_t *par)
   const Double_t arg    = pow(((xx-par[0])/width),2);
   const Double_t ampl  = par[3];
 
-  return ampl*exp(-arg/2) + par[4] + xx*(par[5] + xx * par[6]); 
+  return ampl*exp(-arg/2); 
 }
 
 int findUnique(std::vector<double> v)
@@ -79,9 +79,29 @@ double findMean(std::vector<double> v)
 Double_t myfunc(Double_t *x, Double_t *par)
 {
 	//double f=par[0] + (par[1]*x[0]);
-	double f = par[0];
-	return f;
+	double xx = x[0];
+	double x2 = xx*xx;
+	double x3 = x2*xx;
+	double x4 = x3*xx;
+	double x5 = x4*xx;
+	double x6 = x5*xx;
+	return par[0] + par[1]*xx + par[2]*x2 + par[3]*x3 + par[4]*x4 + par[5]*x5 + par[6]*x6;
 }
+
+//double newtonRaphson(double x, tf1* function, double EPSILON = 0.001) 
+//{ 
+//    double h = tf1->Eval(x) / tf1->Derivative(x); 
+//    while (abs(h) >= EPSILON) 
+//    { 
+//        h = func(x)/derivFunc(x); 
+//   
+//        // x(i+1) = x(i) - f(x) / f'(x)   
+//        x = x - h; 
+//    } 
+//  
+//    cout << "The value of the root is : " << x;
+//   return x; 
+//} 
 
 int main(int argc, char* argv[])
 {
@@ -98,6 +118,7 @@ int main(int argc, char* argv[])
 	std::vector<package> data; 
 
 	double mina(0),minb(0),minc(0), maxa(0), maxb(0), maxc(0);
+	double pmaxa(0),pmaxb(0),pmaxc(0);
 	int posa(0),posb(0),posc(0);
 	
 	for(int i=0; i < num ; i++)
@@ -124,9 +145,9 @@ int main(int argc, char* argv[])
 			std::stringstream ss(line);
 			double d, a, b, c;
 			ss >> d >> std::ws >> a >> std::ws >> b >> std::ws >> c;
-			a = a * scale;
-			b = b * scale;
-			c = c * scale;
+			a = -a * scale;
+			b = -b * scale;
+			c = -c * scale;
 			
 			temp.time.push_back(d);
 			temp.ch1.push_back(a);
@@ -139,31 +160,36 @@ int main(int argc, char* argv[])
 				posa = temp.ch1.size();
 			}	
 			if(a > maxa)
+			{
 				maxa = a;
+				pmaxa = temp.ch1.size();
+			}
 			if(b < minb)
 			{
 				minb = b;
 				posb = temp.ch2.size();
 			}	
 			if(b > maxb)
+			{
 				maxb = b;
+				pmaxb = temp.ch2.size();
+			}
 			if(c < minc)
 			{
 				minc = c;
 				posc = temp.ch3.size();
 			}	
 			if(c > maxc)
+			{
 				maxc = c;
+				pmaxc = temp.ch3.size();
+			}
 
 
 		}
 		data.push_back(temp);
 		file.close();
 	}
-
-
-	/* move minimum to center of histogram */
-
 
 
 	int uniquea(0), uniqueb(0), uniquec(0);
@@ -187,6 +213,7 @@ int main(int argc, char* argv[])
 	std::cout << "mina: " << mina << " minb: " << minb << " minc: " << minc << std::endl;
 	std::cout << "maxa: " << maxa << " maxb: " << maxb << " maxc: " << maxc << std::endl;
 	std::cout << "minimum position a: " << posa << " minimum position b: " << posb << " minimum position c: " << posc << std::endl;
+	std::cout << "maximum position a: " << pmaxa << " maximum position b: " << pmaxb << " maximum position c: " << pmaxc << std::endl;
 
 	int maxunique = 0;
 	if (uniquea > uniqueb)
@@ -198,11 +225,18 @@ int main(int argc, char* argv[])
 	for(int i =0; i < num; i++)
 		scaleTime(data[i].time);
 
+	double timestart = data[0].time[0];
+	double timeend = data[0].time.back();
 	std::cout << "Time start: " << data[0].time[0] << std::endl;
 	std::cout << "Time end: " << data[0].time.back() << std::endl;
 
+	std::vector<float> timech1;
+	std::vector<float> ch1scaled;
+
 	TApplication* rootapp = new TApplication("PMT timing", &argc, argv);
-	TCanvas *c1 = new TCanvas("c1", "t");
+	TFile *file  = new TFile("PMT_fit.root", "RECREATE");
+	
+	TCanvas *c2 = new TCanvas("c2","test");	
 	TH2D *h2 = new TH2D("h2", "PMT-1A response from LED pulse", data[0].time.size(), data[0].time.front(), data[0].time.back(), maxunique , mina, maxa);
 
 	for (int j = 0; j < num; j++)
@@ -217,7 +251,8 @@ int main(int argc, char* argv[])
 	h2->SetOption("scat");
 	h2->GetYaxis()->SetTitle("Voltage [mV]");
 	h2->GetXaxis()->SetTitle("Time [nS]");
-	h2->Draw();
+	h2->Draw();	
+	TCanvas *c1 = new TCanvas("c1","test");	
 	TProfile *p1 = new TProfile("p1", "Profile of CH1", data[0].time.size(), data[0].time.front(), data[0].time.back());
 
 	for (int j = 0; j < num; j++)
@@ -227,37 +262,34 @@ int main(int argc, char* argv[])
 			p1->Fill(data[j].time[i], data[j].ch1[i]);
 		}
 	}	
+
+
       	
 	p1->SetOption("hist");
 	p1->SetMarkerColor(kGreen);
-	p1->Draw("SAME");
+	p1->Draw();
+	double mintimepos = ((TAxis*)p1->GetXaxis())->GetBinCenter(posa);//-1.076E-007+((posa+1)*(4E-10));
+	double maxtimepos = ((TAxis*)p1->GetXaxis())->GetBinCenter(pmaxa);//-1.076E-007+((posa+1)*(4E-10));
+	double maximum(0);
+	for(int i = 1; i < num; i++)
+	{
+		double temp = p1->GetBinContent(i);
+		if (temp > maximum)
+			maximum = temp;
+	}
+	
+	
+	std::cout << "Time at which the function is a maximum: " << maxtimepos << std::endl << "Value of the maximum: " << maximum << std::endl;
+	TF1 *f_Gaus_2sigma = new TF1("f_Gaus_2sigma", Gaus_2sigma, 90, 110, 4 );
+	f_Gaus_2sigma->SetParameters(50, 110, 1, 1);
+	//f_Gaus_2sigma->FixParameter(5,maxtimepos);
+	//f_Gaus_2sigma->FixParameter(0,maximum);
+	p1->Fit("f_Gaus_2sigma");
+	f_Gaus_2sigma->Draw("SAME");
 
-	TProfile *p2 = (TProfile*)p1->Clone();
-	TCanvas* c2 = new TCanvas("c2", "Fit");
-	p2->SetMarkerColor(kBlack);
-	p2->Draw();
-
-
-	//TF1 *f1 = new TF1("fit",myfunc, 88, 92, 2);
-	//f1->SetLineColor(3);
-	//p2->GetXaxis()->SetRange(p2->GetBin(88),p2->GetBin(92));
-	//std::cout << p2->GetMean() << std::endl;
-	//f1->SetParameters(p2->GetMean(),p2->GetRMS());
-	//p2->GetXaxis()->SetRange();	
-	//p2->Fit(f1);
-	//f1->Draw("SAME");
-		
-	TF1 *f_Gaus_2sigma = new TF1("f_Gaus_2sigma", Gaus_2sigma, 0, 250, 7 );
-	f_Gaus_2sigma->SetParameters(0, 100, 0.1, 0.5, 0, 0, 0);
-	p2->Fit(f_Gaus_2sigma);
-	f_Gaus_2sigma->Draw("SAME");	
-
-	TF1 *f_Gaus_lau = new TF1("f_Gaus_lau", gaus_lau, 0, 250, 7 );
-	f_Gaus_lau->SetParameters(10, 0.5, 0.5, 50, 0, 0, 0);
-	f_Gaus_lau->SetLineColor(3);
-	p2->Fit(f_Gaus_lau);
-	f_Gaus_lau->Draw("SAME");	
 	rootapp->Run();	
+	file->Write();	
+	file->Close();
 
 	return 0;
 }
