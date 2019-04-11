8a7f84b2348de38c8dc0048aedde6e48cbcc2ec6
Nick Ristow <nristow@umich.edu>
2019-04-11 15:15:25 -0400

    too many constants, one was removed

--- fermidirac.cpp
+++ fermidirac.cpp
@@ -11,3 +11,17 @@ double fermi_dirac(double *x, double *par)
 	z = A/z;
 	return z;
 }
+
+double fermi_dirac_parabola(double *x, double *par)
+{
+	double t = x[0];
+	double alpha = par[0];
+	double t0 = par[1];
+	double A = par[2];
+        double c1 = par[3];
+        double c2 = par[4];
+        //double c3 = par[5];
+        
+        double z = 1+exp((t0-t)/alpha);
+        return (A+ c2*t + c1*t*t)/z;
+}
