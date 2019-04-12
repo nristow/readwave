594ffc2b39539294d0bd1246b58966ba1752f2c9
Nick Ristow <nick.ristow@gmail.com>
2019-04-12 12:49:35 -0400

    macro for fitting

--- fermidirac_macro.cxx
+++ fermidirac_macro.cxx
@@ -17,14 +17,16 @@ double fermi_dirac_parabola(double *x, double *par)
 	double alpha = par[0];
 	double t0 = par[1];
 	double A = par[2];
-        double B = par[3];
-        double C = par[4];
-        double D = par[5];
-
-	double z = 1+exp((t0-t)/alpha);
-	z = A/z;
-	return z;
+        double c1 = par[3];
+        double c2 = par[4];
+        double c3 = par[5];
+        
+        double z = 1+exp((t0-t)/alpha);
+        return (A+c3 + c2*t + c1*t*t)/z;
 }
 
-TF1 *f_fit = new TF1("fermi_dirac", fermi_dirac, 0, 9, 3);
+TF1 *ttttt = new TF1("fermi_dirac", fermi_dirac, 0, 9, 3);
+TF1 *f1 = new TF1("fermi_dirac_parabola", fermi_dirac_parabola, 0, 9.5, 6);
+//f1->SetParNames("Alpha","t0","A", "c1", "c2", "c3");
+//f1->SetParameters(1.0,8,108, 0, 0, 0);
 
